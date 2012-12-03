8cd2ce09e6c2b04a9725eda1cace7c056008fb55
Danilo Casagrande de Almeida <dangras.almeida@gmail.com>
2012-12-03 21:00:12 -0200

    Front End Updates - Model Insert / Model Composite View

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -67,11 +67,40 @@ $(document).ready(function(){
 			
 			if( (objYear - optYear) < 18 ){
 				$('div.model-input-responsible').slideDown(400);
+				$('div.model-input-responsible div.put-left:eq(0)').addClass('field-isreq');
+				$('div.model-input-responsible div.put-left:eq(1)').addClass('field-isreq');
+				$('div.model-input-responsible div.put-left:eq(2)').addClass('field-isreq');
 			}else{
 				$('div.model-input-responsible').slideUp(400);
+				$('div.model-input-responsible div.put-left:eq(0)').removeClass('field-isreq');
+				$('div.model-input-responsible div.put-left:eq(1)').removeClass('field-isreq');
+				$('div.model-input-responsible div.put-left:eq(2)').removeClass('field-isreq');
 			}
 			
 		});
+	}
+	
+	function externaModelAge(){
+		var objDate = new Date();
+		var objYear = objDate.getFullYear();
+		
+		var optYear = $('div#admin-model-insert-basic-info select#model_birthday_1i').val();
+		
+		if( (objYear - optYear) < 18 ){
+			$('div.model-input-responsible').slideDown(400);
+		}else{
+			$('div.model-input-responsible').slideUp(400);
+		}
+	}
+	
+	function checkModelAgeBox(){
+		var hasVal = "";
+		
+		for( p in $('div.model-input-responsible input') ){
+			hasVal = $('div.model-input-responsible input:eq('+p+')').val();
+			if( hasVal != "" ){ $('div.model-input-responsible').slideDown(400); }
+		}
+		
 	}
 	
 	function mapModelMeasures(){
@@ -93,6 +122,122 @@ $(document).ready(function(){
 		$('a#btn-model-hide-data').click(function(event) {
 			$('div#model-secondary-info').slideUp(400);
 		});
+		
+		var hasComposite = $('div#model-info-composite').size();
+		
+		if( hasComposite == 1 ){
+			
+			$('#model-info-composite div#composite-cover img').resizecrop({
+				width:400,
+			    height:560,
+			    vertical:"top"
+			});
+			
+			$('#model-info-composite div.composite-thumbs img').resizecrop({
+				width:190,
+				height:220,
+				vertical:"top"
+			});
+			
+		};
+		
+		var hasGallery = $('div#model-photo-gallery ul#model-gallery').size();
+		var modelGalleryImg = $('div#model-photo-gallery ul#model-gallery li img');
+		
+		if( hasGallery == 1 ){
+			
+			for( p in modelGalleryImg ){
+				$('div#model-photo-gallery ul#model-gallery li img:eq('+p+')').css("width","auto");
+				$('div#model-photo-gallery ul#model-gallery li img:eq('+p+')').css("height","auto");
+				$('div#model-photo-gallery ul#model-gallery li img:eq('+p+')').resizecrop({
+					width:140,
+					height:140
+				});
+			}
+			
+		}
+		
+	}
+	
+	function modelFormValidation(){
+		
+		if( $('form#new_model').length != 0 ){
+			
+			var form = $('form#new_model');
+			var formInputsCount = form.find("input").length;
+			var formInputSubmit;
+
+			var reqCount;
+			var fieldInput;
+			var fieldVal;
+			var fieldContainer;
+			var sendForm;
+
+			var defaultErrorMsg = '<div class="field-error-msg"><p>* Este campo é orbigatório.</p></div>';
+
+			//Store the SUBMIT Btn Inside Variable
+			for (var i = 0; i < formInputsCount; i++) {
+				if ( form.find("input:eq("+i+")").attr("type") == "submit" ){
+					formInputSubmit = form.find("input:eq("+i+")");
+				};
+			};
+
+			//Check How Many are required - div.form-isreq
+			formInputSubmit.click(function(event) {
+
+				reqCount = form.find("div.field-isreq").length;
+				sendForm = new Array(reqCount);
+
+				for (var p = 0; p < reqCount; p++) {
+
+					fieldContainer = form.find("div.field-isreq:eq("+p+")");
+
+					if( form.find("div.field-isreq:eq("+p+") input").length == 1 ){
+						fieldInput = form.find("div.field-isreq:eq("+p+") input");
+						fieldVal = form.find("div.field-isreq:eq("+p+") input").val();
+					}else{
+						fieldInput = form.find("div.field-isreq:eq("+p+") select");
+						fieldVal = form.find("div.field-isreq:eq("+p+") select").val();
+					};
+
+					if( fieldVal == "" || fieldVal == undefined || fieldVal == "undefined" ){
+
+						fieldContainer.addClass('field-isreq-active');
+
+						if( form.find("div.field-isreq:eq("+p+") div.field-error-msg").length == 0 ){
+							fieldContainer.append(defaultErrorMsg);
+						}
+
+						sendForm[p] = false;
+
+					}else{
+
+						fieldContainer.removeClass('field-isreq-active');
+						sendForm[p] = true;
+
+						if( form.find("div.field-isreq:eq("+p+") div.field-error-msg").length == 1 ){
+							fieldContainer.find('div.field-error-msg').remove();
+						};
+
+					}
+
+				};
+
+				for (var m = 0; m < sendForm.length; m++) {
+
+					if( !sendForm[m] ){
+						externaModelAge();
+						return false;
+						break;
+					}else if( m == ( sendForm.length - 1 ) &&  sendForm[m] ){
+						return true;
+					}
+
+				};
+
+			});
+			
+		}
 	}
 	
 	btnFolderHover();
@@ -101,6 +246,8 @@ $(document).ready(function(){
 	mapModelAge();
 	mapModelMeasures();
 	modelShowSecondInfo();
+	modelFormValidation();
+	checkModelAgeBox();
 
 	//loginPanelSetup();
 	
