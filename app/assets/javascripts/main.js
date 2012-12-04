e27aa4454fd4858cac877717209ce82e27115f0d
Danilo Casagrande de Almeida <dangras.almeida@gmail.com>
2012-12-04 19:06:24 -0200

    Front End Update - Major Update on Casting Menu Nav - Composite Nav

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -240,6 +240,32 @@ $(document).ready(function(){
 		}
 	}
 	
+	function setupCompositeNav(){
+		
+		var hasModelsResult = $('div#search-models-result').length;
+		var hasCastingResult = $('div#casting-models-result').length;
+		
+		if( hasModelsResult > 0 || hasCastingResult > 0 ){
+			
+			var modelBoxCount = $('div.model-box').size();
+			var currentID = 0;
+			
+			$('div.model-box li.model-box-view-composite a').click(function(event) {
+				
+				for (var i = 0; i < modelBoxCount; i++) {
+					$('div.model-box:eq('+i+')').removeClass('composite-open');
+				};
+				
+				currentID = $(this).parents('div.model-box').index();
+				$('div.model-box:eq('+currentID+')').addClass('composite-open');
+				
+			});
+			
+		};
+		
+	}
+	
+		
 	btnFolderHover();
 	dropdownMenuNav();
 	searchAdvancedOpts();
@@ -248,6 +274,7 @@ $(document).ready(function(){
 	modelShowSecondInfo();
 	modelFormValidation();
 	checkModelAgeBox();
+	setupCompositeNav();
 
 	//loginPanelSetup();
 	
