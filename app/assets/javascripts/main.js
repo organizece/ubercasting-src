af4882c1b3beef8cbf035fe775d8834bbd0d82c6
Danilo Casagrande de Almeida <dangras.almeida@gmail.com>
2012-12-13 18:59:42 -0200

    Aplication Updates - Model Insert Form / Model Casting View / Model Search View / Model Composite Nav / Model Casting Vote System / Testimonial Fields

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -244,32 +244,6 @@ $(document).ready(function(){
 		}
 	}
 	
-	function setupCompositeNav(){
-		
-		var hasModelsResult = $('div#search-models-result').length;
-		var hasCastingResult = $('div#casting-models-result').length;
-		
-		if( hasModelsResult > 0 || hasCastingResult > 0 ){
-			
-			var modelBoxCount = $('div.model-box').size();
-			var currentID = 0;
-			
-			$('div.model-box li.model-box-view-composite a').click(function(event) {
-				
-				for (var i = 0; i < modelBoxCount; i++) {
-					$('div.model-box:eq('+i+')').removeClass('composite-open');
-				};
-				
-				currentID = $(this).parents('div.model-box').index();
-				$('div.model-box:eq('+currentID+')').addClass('composite-open');
-				
-			});
-			
-		};
-		
-	}
-	
-		
 	btnFolderHover();
 	dropdownMenuNav();
 	searchAdvancedOpts();
@@ -278,7 +252,6 @@ $(document).ready(function(){
 	modelShowSecondInfo();
 	modelFormValidation();
 	checkModelAgeBox();
-	setupCompositeNav();
 
 	//loginPanelSetup();
 	
