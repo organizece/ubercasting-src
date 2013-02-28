a455622b63715d42fd4043163af474808a7017ed
Bruno Biasetti <bruno.biasetti@gmail.com>
2013-02-27 23:19:54 -0300

    DEV - CUSTOMER - Acesso cliente entre agências
    Fix p/ também exibir site aberto, e não total_access considerando em
    conta associação do cliente com agência.

--- app/controllers/subdomain_models_controller.rb
+++ app/controllers/subdomain_models_controller.rb
@@ -4,9 +4,9 @@ class SubdomainModelsController < ApplicationController
   def index
     @website = Website.find_by_subdomain(params[:subdomain])
     @models = Model.search(ModelSearchCriteria.build_criteria(params, @website.agency))
-    #TODO arrumar p/ levar em consideração cliente associado a agencia
+
     @visualization_mode = @website.visualization_mode
-    @visualization_mode = 'total_access' if customer_signed_in?
+    @visualization_mode = 'total_access' if customer_total_access?
 
     @models_ids = []
     @models.each do |model|
@@ -18,12 +18,14 @@ class SubdomainModelsController < ApplicationController
   end
 
   def show
+    #TODO Melhoria: Validar se cliente realmente deveria ter acesso (logado ou site aberto)
     @website = Website.find_by_subdomain(params[:subdomain])
     @model = @website.agency.models.find(params[:id])
   end
   
   def composite
     @composite = Model.find(params[:id]).composite
+    @total_access = customer_total_access?
 
     respond_to do |format|
       format.js
@@ -50,6 +52,20 @@ private
 
   def per_page
     ITENS_PER_PAGE.include?(params[:itens_per_page]) ? params[:itens_per_page] : 6
+  end
+
+  def customer_total_access?
+    total_access = false
+    
+    if customer_signed_in?
+      # If customer is signed in validate if it has an association with the agency
+      website = Website.find_by_subdomain(params[:subdomain])
+      agency_customer = AgencyCustomer.find_by_agency_id_and_customer_id(website.agency.id, current_customer.id)
+
+      total_access = true if agency_customer
+    end
+
+    total_access
   end
 
 end
