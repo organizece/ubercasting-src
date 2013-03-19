e82d31890e113ea14c5fb1cf6ad8cc96a6c519d6
Bruno Biasetti <danilo@organizece.com>
2013-03-18 23:27:34 -0300

    Commit p/ subida no Heroku - Geracao de PDF

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130308015559) do
+ActiveRecord::Schema.define(:version => 20130308225353) do
 
   create_table "agencies", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
