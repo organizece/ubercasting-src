2c6a6d29a6e3120ef8d6af2e016bd792b58055bd
Bruno Biasetti <bruno.biasetti@gmail.com>
2013-05-11 11:53:48 -0300

    Crop test 5

--- app/controllers/models_controller.rb
+++ app/controllers/models_controller.rb
@@ -165,13 +165,15 @@ class ModelsController < ApplicationController
 
   def save_profile_pic
     @model = Model.find(params[:model_id])
+    logger.debug 'params[:avatar_url].split(?)[0]'
+    logger.debug params[:avatar_url].split('?')[0]
     @model.avatar_from_url(params[:avatar_url].split('?')[0])
-    @model.save!
+    # @model.save!
 
-    @model.crop_x = params[:crop_x]
-    @model.crop_y = params[:crop_y]
-    @model.crop_h = params[:crop_h]
-    @model.crop_w = params[:crop_w]
+    # @model.crop_x = params[:crop_x]
+    # @model.crop_y = params[:crop_y]
+    # @model.crop_h = params[:crop_h]
+    # @model.crop_w = params[:crop_w]
 
     respond_to do |format|
       if @model.save!
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130422191747) do
+ActiveRecord::Schema.define(:version => 20130511140133) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -256,6 +256,10 @@ ActiveRecord::Schema.define(:version => 20130422191747) do
     t.integer  "mannequin"
     t.string   "video"
     t.string   "art_name"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "models_specialties", :force => true do |t|
