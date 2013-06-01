f2c680fc69c0a77155ab45ad407d85ffb9948290
Danilo Casagrande de Almeida <dangras.almeida@gmail.com>
2013-06-01 11:14:25 -0300

    Small Front End Updates

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130531140309) do
+ActiveRecord::Schema.define(:version => 20130531144111) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -47,12 +47,12 @@ ActiveRecord::Schema.define(:version => 20130531140309) do
   add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
 
   create_table "agencies", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "email",                          :default => "", :null => false
+    t.string   "encrypted_password",             :default => "", :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
+    t.integer  "sign_in_count",                  :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -64,8 +64,8 @@ ActiveRecord::Schema.define(:version => 20130531140309) do
     t.string   "cnpj"
     t.string   "owner_name"
     t.string   "owner_cpf"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at",                                     :null => false
+    t.datetime "updated_at",                                     :null => false
     t.string   "account_type"
     t.string   "account_period"
     t.string   "account_payment"
@@ -91,6 +91,8 @@ ActiveRecord::Schema.define(:version => 20130531140309) do
     t.integer  "subscription_id"
     t.float    "latitude"
     t.float    "longitude"
+    t.string   "paypal_customer_token"
+    t.string   "paypal_recurring_profile_token"
   end
 
   add_index "agencies", ["confirmation_token"], :name => "index_agencies_on_confirmation_token", :unique => true
