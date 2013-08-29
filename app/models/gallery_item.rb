fdfed50db5d314379f8539a7cb8dcaad9adbc98a
Bruno Biasetti <bruno.biasetti@gmail.com>
2013-08-28 22:04:12 -0300

    Serach galeria + telas

--- app/models/gallery_item.rb
+++ app/models/gallery_item.rb
@@ -1,7 +1,8 @@
 class GalleryItem < ActiveRecord::Base
   attr_accessible :description, :title, :image, :agency_id
 
-  has_attached_file :image, styles: { thumb: "140x140>" }
+  has_attached_file :image, styles: { thumb: "600x512>",
+                                      gallery: "960x768>" }
 
   belongs_to :agency
 
